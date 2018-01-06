5f12999687d675cd1e36c407597c1eb6cc75e6a3
Shane Honanie <shaneh425@gmail.com>
2018-01-05 18:28:48 -0800

    Passport OAuth FB

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '505011409835718',
+        clientSecret: 'c7e45e518fb583a0ee1a3202e36ad5e8'
+    }
 }
